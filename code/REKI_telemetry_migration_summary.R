cf349f5a1f9599742e796277416a6ad664d40221
Steffen Oppel <steffen.oppel@gmail.com>
2024-12-04 13:56:46 +0100

    summarising telemetry movements

--- code/REKI_telemetry_migration_summary.R
+++ code/REKI_telemetry_migration_summary.R
@@ -70,6 +70,72 @@ REKI_summary<-REKI %>%
   summarise(n=length(unique(id)))
 write.csv(REKI_summary, "REKI_grid_summary.csv")
 
+REKI_ind_summary<-REKI %>% 
+  mutate(mo=month(timestamp), day=day(timestamp)) %>%
+  mutate(start=if_else(day<11,ymd(paste("2020",mo,"01", sep="-")),
+                       if_else(day<21,ymd(paste("2020",mo,"11", sep="-")),ymd(paste("2020",mo,"21", sep="-"))))) %>%
+  left_join(timebins, by="start") %>%
+  st_transform("+proj=eqearth +datum=WGS84 +lon_0=-10") %>%
+  st_join(grid_EU_REKI,join = st_within) %>%
+  st_drop_geometry() %>%
+  group_by(ID, id, period_id) %>%
+  summarise(n=length(timestamp))
+
+## for each period and individual take the hexagon with the max number of locations
+REKI_migra_links<-expand.grid(id=unique(REKI_ind_summary$id),period_id=timebins$period_id) %>%
+  mutate(origin=0,destination=0)
+for (i in unique(REKI_ind_summary$id)){
+  for(t in 1:max(timebins$period_id)){
+    xi<-REKI_ind_summary %>%
+      filter(id==i) %>%
+      filter(period_id==t)
+    if(dim(xi)[1]>0){
+      REKI_migra_links$origin[REKI_migra_links$id==i & REKI_migra_links$period_id==t]<-xi$ID[which.max(xi$n)]
+      xit<-REKI_ind_summary %>%
+        filter(id==i) %>%
+        filter(period_id==ifelse(t==36,1,t+1))
+      if(dim(xit)[1]>0){ 
+      REKI_migra_links$destination[REKI_migra_links$id==i & REKI_migra_links$period_id==t]<-xi$ID[which.max(xi$n)]
+      }
+    }
+  }
+}
+
+
+REKI_migra_links_plot<-REKI_migra_links %>%
+  filter(origin>0) %>%
+  filter(destination>0) %>%
+  group_by(period_id,origin,destination) %>%
+  summarise(flow=length(unique(id))/length(unique(REKI_ind_summary$id))) %>%
+  mutate(
+    x1 = as.numeric(hex_centroids$X[origin]),
+    x2 = as.numeric(hex_centroids$X[destination]),
+    y1 = as.numeric(hex_centroids$Y[origin]),
+    y2 = as.numeric(hex_centroids$Y[destination])) %>%
+  left_join(timebins, by="period_id") %>%
+  select(-period,-end) %>%
+  arrange(start,flow)
+
+
+ggplot() +
+  geom_sf(data=migration_system_map) +
+  geom_sf(data = grid_EU_plot, aes(fill = n), alpha=0.6) +
+  scale_fill_continuous(type="viridis") +
+  geom_segment(data = REKI_migra_links_plot,
+               aes(x = x1, y = y1, xend = x2, yend = y2, linewidth=flow),
+               arrow = ggplot2::arrow(length = unit(5, 'mm'))) +
+  facet_wrap(~start) +
+  scale_linewidth(range=c(0,max(REKI_migra_links_plot$flow)), name = "Migration flow") +
+  theme(plot.title = element_text(size=30),
+        legend.text = element_text(size = 30), 
+        legend.title = element_text(size = 30), 
+        legend.key.size = unit(2, 'cm'))
+
+
+
+
+
+
 
 # Plot the grid over Europe
 grid_EU_plot <- grid_EU_REKI %>% full_join(REKI_summary, by='ID') %>%
@@ -89,7 +155,82 @@ ggsave("REKI_annual_distribution_telemetry.jpg", width=9, height=8)
   
 ##  Potential spatiotemporal paths across hexagons  ##
 
+# Define filtering functions
+filtering_fct <- function(x){
+  x_delta <- x[2:length(x)] - x[1:(length(x)-1)]
+  
+  tokeep0 = tokeep1 = tokeep2 = tokeep3 = 1
+  
+  # Avoid zigzags
+  if(length(x) > 2){
+    if(x_delta[length(x_delta)] != 0 & x_delta[length(x_delta)-1] != 0){
+      if(x[length(x)] %in% hexgrid_neighbours[[x[length(x)-2]]]){
+        tokeep0 = 0
+      } 
+    }
+  }
+  
+  # Has there been movement previously?
+  filter_1 <- ifelse(sum(x_delta[-length(x_delta)] > 0) > 0, 1, 0)
+  filter_2 <- ifelse(sum(x_delta[-length(x_delta)] < 0) > 0, 1, 0)
+  
+  if(filter_1 == 1 & filter_2 == 0){
+    # Individual cannot start migrating back before spending Smin weeks on seasonal grounds
+    if(x_delta[length(x_delta)] < 0){
+      if((length(x_delta)-max(which(x_delta > 0))) < Smin){
+        tokeep1 = 0
+      }
+    }
+    # Individual cannot stop during migration for more than one 10-day period
+    if(x_delta[length(x_delta)] > 0){
+      if(x_delta[length(x_delta)-1] <= 0){
+        tokeep1 = 0
+      }
+    }
+  }
+  
+  if(filter_1 == 0 & filter_2 == 1){
+    # Individual cannot start migrating back before spending Smin weeks on seasonal grounds
+    if(x_delta[length(x_delta)] > 0){
+      if((length(x_delta)-max(which(x_delta < 0))) < Smin){
+        tokeep2 = 0
+      }
+    }
+    # Individual cannot stop during migration for more than one 10 day period
+    if(x_delta[length(x_delta)] < 0){
+      if(x_delta[length(x_delta)-1] >= 0){
+        tokeep2 = 0
+      }
+    }
+  }
+  
+  # Individual has to do the same migration back and forth (need to end up in the same place)
+  if(filter_1 == 1 & filter_2 == 1){
+    if(x_delta[length(x_delta)] == 0 & x[length(x)] != x[1]){
+      tokeep3 = 0
+    }
+    if(x_delta[length(x_delta)] < 0 & x[length(x)] < x[1]){
+      tokeep3 = 0
+    }
+    if(x_delta[length(x_delta)] > 0 & x[length(x)] > x[1]){
+      tokeep3 = 0
+    }
+  }
+  
+  tokeep4 <- tokeep0 * tokeep1 * tokeep2 * tokeep3
+  return(tokeep4)
+}
+
+filtering_fct_2 <- function(x){
+  tokeep2 <- ifelse(length(which(x == max(x))) >= 1 & length(which(x == min(x))) >= 1, 1, 0)
+  return(tokeep2)
+}
+
 # Algorithm to create the spatiotemporal network
+# Set parameter values
+timesteps = max(timebins$period_id)  # number of time steps throughout the time frame of the model
+Smin = 1  # minimum number of time steps to stay at destinations
+Nreg = nrow(grid_EU_REKI)  # number of geographical bands
 
 # Set parameter values
 hexgrid <- grid_EU_REKI
@@ -108,7 +249,7 @@ spatiotemporal_paths <- list()
 for(k in 1:Nreg){
   s = list() 
   s[[1]] <- k
-  for(i in 2:timesteps){
+  for(i in 2:max(timebins$period_id)){
     # which hexagon is touching the last occupied hexagons
     #s[[i]] <- unlist(hexgrid_neighbours[s[[i-1]]])
     aaa <- hexgrid_neighbours[s[[i-1]]]
@@ -124,7 +265,7 @@ for(k in 1:Nreg){
   }
   spat_paths <- do.call(cbind,s)
   # remove paths for which the last hexagon is not a neighbour of the first hexagon (or itself)
-  tokeep <- spat_paths[,timesteps] %in% hexgrid_neighbours[[k]]
+  tokeep <- spat_paths[,i] %in% hexgrid_neighbours[[k]]
   #tokeep <- vector()
   #for(i in 1:nrow(spat_paths)){
   #  tokeep[i] <- spat_paths[,timesteps][i] %in% unlist(hexgrid_neighbours[spat_paths[,1][i]])
@@ -136,15 +277,16 @@ for(k in 1:Nreg){
   print(c(k, nrow(spatiotemporal_paths[[k]])))
 }
 spatiotemporal_paths <- do.call(rbind, spatiotemporal_paths)
-write.csv(spatiotemporal_paths, "resources/spatiotemporal_paths_hex3.csv")
+write.csv(spatiotemporal_paths, "C:/Users/sop/OneDrive - Vogelwarte/General/ANALYSES/REKI_NDVI/output/spatiotemporal_paths_hex3.csv")
 
 
 ## Graph model [deleted code except the code that specifies structure of output data frame]
 
-graph_paths <- as.matrix(read.csv("resources/spatiotemporal_paths_hex3.csv", header = T)[,-1])
-ee_path_ID <- which.max(ee_score)
-simulated_migr_paths <- c(simulated_migr_paths, ee_path_ID)
-simulated_migration_paths <- graph_paths[simulated_migr_paths,]
+graph_paths <- as.matrix(read.csv("C:/Users/sop/OneDrive - Vogelwarte/General/ANALYSES/REKI_NDVI/output/spatiotemporal_paths_hex3.csv", header = T)[,-1])
+# ee_path_ID <- which.max(ee_score)
+# simulated_migr_paths <- vector()
+# simulated_migr_paths <- c(simulated_migr_paths, ee_path_ID)
+simulated_migration_paths <- graph_paths
 
 
 ## Plot results from STDS model ##
@@ -165,41 +307,6 @@ for(t in 1:timesteps){
   }
 }
 
-# Total weekly migration activity 
-total_migration_activity <- vector()
-for(i in 1:timesteps){
-  total_migration_activity[i] <- sum(as.numeric(migra_links$Freq[which(migra_links$t==i)]))
-}
-data_to_plot <- data.frame(date = as.Date(seq(1, 360, 10), origin="2015-01-01"),
-                           migra_activity = total_migration_activity)
-theme_set(theme_bw())
-ggplot(data_to_plot, aes(x=date, y=migra_activity)) + 
-  geom_line() + 
-  geom_point() + xlab("Date") + ylab("Migration activity")
-
-# Seasonal difference in richness
-weekly_density <- vector()
-for(i in 1:timesteps){
-  weekly_dens <- vector()
-  for(j in 1:Nreg){
-    weekly_dens[j] <- length(which(simulated_migration_paths[,i] == j))
-  }
-  weekly_density <- rbind(weekly_density, weekly_dens)
-}
-winter_timestep <- 1
-summer_timestep <- 22
-density_winter <- weekly_density[winter_timestep,]
-density_summer <- weekly_density[summer_timestep,]
-data_plot <- data.frame(band = 1:13,
-                        winter_density = density_winter,
-                        summer_density = density_summer,
-                        seasonal_difference = density_summer - density_winter)
-theme_set(theme_classic())
-ggplot(data_plot) +
-  geom_smooth(aes(x=band, y=seasonal_difference),span = 0.5) +
-  geom_point(aes(x=band, y=seasonal_difference)) +
-  xlab("Latitude") + ylab("Seasonal difference in bird density")
-
 # Plot weekly links on map
 migra_links2 <- cbind(migra_links, t(apply(migra_links, 1, function(x) as.numeric(unlist(strsplit(as.character(x[2]), " "))))))
 colnames(migra_links2) <- c("week", "unique_ID", "inds", "origin", "destination")
@@ -209,38 +316,68 @@ myTheme <- theme(plot.title = element_text(size=30),
                  legend.text = element_text(size = 30), 
                  legend.title = element_text(size = 30), 
                  legend.key.size = unit(2, 'cm'))
-g_flow_bins <- list()
-for(t in 1:timesteps){
-  migra_links_w <- migra_links2 %>% filter(week==t)
-  migra_links_w_plot <- data.frame(
-    x1 = as.numeric(hex_centroids$X[migra_links_w$origin]),
-    x2 = as.numeric(hex_centroids$X[migra_links_w$destination]),
-    y1 = as.numeric(hex_centroids$Y[migra_links_w$origin]),
-    y2 = as.numeric(hex_centroids$Y[migra_links_w$destination]),
-    flow = as.numeric(migra_links_w$inds)
-  )
-  g_flow_bins[[t]] <- ggplot() +
-    geom_sf(data=migration_system_map) +
-    geom_sf(data = grid_EU[selected_hexs_distrib,], fill = "firebrick", alpha=0.3) + theme_void() + xlim(c(-15500, 3000000)) +
-    ggtitle(format(ymd(paste0("15", mois[floor(t/3)+1], jours[round(((t/3) - floor(t/3)) *3) + 1])), "%B %d")) +
-    geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2, linewidth=flow),
-                 data = migra_links_w_plot[order(migra_links_w_plot$flow),],
-                 #lineend = "round", linejoin = "round",
-                 arrow = ggplot2::arrow(length = unit(0.04, "npc"))) + myTheme +
-    scale_linewidth(range=c(0,ifelse(nrow(migra_links_w_plot) > 0, max(migra_links_w_plot$flow)/3, 1)), name = "Migration flow")
-}
 
-pdf(file=paste0("results/flow_results.pdf"), width=45, height=45)
-ggarrange(g_flow_bins[[1]], g_flow_bins[[2]], g_flow_bins[[3]], g_flow_bins[[4]], g_flow_bins[[5]], g_flow_bins[[6]],
-          g_flow_bins[[7]], g_flow_bins[[8]], g_flow_bins[[9]], g_flow_bins[[10]], g_flow_bins[[11]], g_flow_bins[[12]],
-          g_flow_bins[[13]], g_flow_bins[[14]], g_flow_bins[[15]], g_flow_bins[[16]], g_flow_bins[[17]], g_flow_bins[[18]],
-          g_flow_bins[[19]], g_flow_bins[[20]], g_flow_bins[[21]], g_flow_bins[[22]], g_flow_bins[[23]], g_flow_bins[[24]],
-          g_flow_bins[[25]], g_flow_bins[[26]], g_flow_bins[[27]], g_flow_bins[[28]], g_flow_bins[[29]], g_flow_bins[[30]],
-          g_flow_bins[[31]], g_flow_bins[[32]], g_flow_bins[[33]], g_flow_bins[[34]], g_flow_bins[[35]], g_flow_bins[[36]],
-          nrow=6, ncol=6, common.legend = T, legend="none")
-dev.off()
+### original approach in a loop
+# g_flow_bins <- list()
+# for(t in 1:timesteps){
+#   migra_links_w <- migra_links2 %>% filter(week==t)
+#   migra_links_w_plot <- data.frame(
+#     x1 = as.numeric(hex_centroids$X[migra_links_w$origin]),
+#     x2 = as.numeric(hex_centroids$X[migra_links_w$destination]),
+#     y1 = as.numeric(hex_centroids$Y[migra_links_w$origin]),
+#     y2 = as.numeric(hex_centroids$Y[migra_links_w$destination]),
+#     flow = as.numeric(migra_links_w$inds)
+#   )
+#   g_flow_bins[[t]] <- ggplot() +
+#     geom_sf(data=migration_system_map) +
+#     geom_sf(data = grid_EU[selected_hexs_distrib,], fill = "firebrick", alpha=0.3) + theme_void() + xlim(c(-15500, 3000000)) +
+#     ggtitle(format(timebins$start[timebins$period_id==t], "%B %d")) +
+#     geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2, linewidth=flow),
+#                  data = migra_links_w_plot[order(migra_links_w_plot$flow),],
+#                  #lineend = "round", linejoin = "round",
+#                  arrow = ggplot2::arrow(length = unit(0.04, "npc"))) + myTheme +
+#     scale_linewidth(range=c(0,ifelse(nrow(migra_links_w_plot) > 0, max(migra_links_w_plot$flow)/3, 1)), name = "Migration flow")
+# }
+# 
+# pdf(file=paste0("results/flow_results.pdf"), width=45, height=45)
+# ggarrange(g_flow_bins[[1]], g_flow_bins[[2]], g_flow_bins[[3]], g_flow_bins[[4]], g_flow_bins[[5]], g_flow_bins[[6]],
+#           g_flow_bins[[7]], g_flow_bins[[8]], g_flow_bins[[9]], g_flow_bins[[10]], g_flow_bins[[11]], g_flow_bins[[12]],
+#           g_flow_bins[[13]], g_flow_bins[[14]], g_flow_bins[[15]], g_flow_bins[[16]], g_flow_bins[[17]], g_flow_bins[[18]],
+#           g_flow_bins[[19]], g_flow_bins[[20]], g_flow_bins[[21]], g_flow_bins[[22]], g_flow_bins[[23]], g_flow_bins[[24]],
+#           g_flow_bins[[25]], g_flow_bins[[26]], g_flow_bins[[27]], g_flow_bins[[28]], g_flow_bins[[29]], g_flow_bins[[30]],
+#           g_flow_bins[[31]], g_flow_bins[[32]], g_flow_bins[[33]], g_flow_bins[[34]], g_flow_bins[[35]], g_flow_bins[[36]],
+#           nrow=6, ncol=6, common.legend = T, legend="none")
+# dev.off()
 
 
 
 
+### ALTERNATIVE ATTEMPT TO VISUALISE MIGRATION FLOW ###
 
+migra_links_plot <- migra_links2 %>%
+  mutate(
+  x1 = as.numeric(hex_centroids$X[origin]),
+  x2 = as.numeric(hex_centroids$X[destination]),
+  y1 = as.numeric(hex_centroids$Y[origin]),
+  y2 = as.numeric(hex_centroids$Y[destination]),
+  flow = as.numeric(inds/10000)) %>%
+  rename(period_id=week) %>%
+  left_join(timebins, by="period_id") %>%
+  select(-period,-end,-inds) %>%
+  arrange(start,flow) %>%
+  filter(period_id==15)
+
+ggplot() +
+  geom_sf(data=migration_system_map) +
+  geom_sf(data = grid_EU_plot[grid_EU_plot$period_id==15,], aes(fill = n), alpha=0.6) +
+  scale_fill_continuous(type="viridis") +
+  geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2, linewidth=flow),
+               data = migra_links_plot,
+               arrow = ggplot2::arrow(length = unit(0.04, "npc"))) +
+  facet_wrap(~start) +
+  scale_linewidth(range=c(0,max(migra_links_plot$flow), 1), name = "Migration flow") +
+  theme(plot.title = element_text(size=30),
+        legend.text = element_text(size = 30), 
+        legend.title = element_text(size = 30), 
+        legend.key.size = unit(2, 'cm'))
+  
